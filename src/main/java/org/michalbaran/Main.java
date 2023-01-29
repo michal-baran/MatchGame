d23f4d368675dc4d665c4d757e44504bf477cf05
Michal Baran <michal.baran.dev@gmail.com>
2023-01-29 17:11:00 +0100

    Creating ENUMs: Character & AnsiColor

--- src/main/java/org/michalbaran/Main.java
+++ src/main/java/org/michalbaran/Main.java
@@ -1,7 +1,19 @@
 package org.michalbaran;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
 
+        List<Character> characterList = new ArrayList<>();
+        characterList.add(Character.DRA);
+        characterList.add(Character.HAR);
+        characterList.add(Character.DUM);
+        characterList.add(Character.HER);
+
+        for (Character c : characterList) {
+            System.out.print(c + " ");
+        }
     }
 }
