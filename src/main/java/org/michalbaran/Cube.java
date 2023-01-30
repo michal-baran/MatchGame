722a8452f8ccba45f370ca711f0f4c896600e237
Michal Baran <michal.baran.dev@gmail.com>
2023-01-30 17:39:40 +0100

    Input data: cubes defined in txt file

---
+++ src/main/java/org/michalbaran/Cube.java
@@ -0,0 +1,4 @@
+package org.michalbaran;
+
+public class Cube {
+}
--- src/main/java/org/michalbaran/Main.java
+++ src/main/java/org/michalbaran/Main.java
@@ -1,18 +1,8 @@
 package org.michalbaran;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
-
-        List<Character> characterList = new ArrayList<>();
-        characterList.add(Character.DRA);
-        characterList.add(Character.HAR);
-        characterList.add(Character.DUM);
-        characterList.add(Character.HER);
-
-        for (Character c : characterList) {
+        for (Character c : Character.values()) {
             System.out.print(c + " ");
         }
     }
