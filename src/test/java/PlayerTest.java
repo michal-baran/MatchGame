5241b6b5ee0741bf2c93fe8d9d4d0c4269b56446
Michal Baran <michal.baran.dev@gmail.com>
2023-03-16 23:15:52 +0100

    Player tests

--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -1,11 +1,48 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.michalbaran.components.Player;
+import org.michalbaran.enums.Symbol;
+
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class PlayerTest {
+    Player player;
 
-    //Getters
-    @Test
-    public void testPartOne() {
-
+    @BeforeEach
+    void setUp() {
+        player = new Player("First player");
     }
 
+    @Test
+    public void testNewPlayer() {
+        Player player = new Player("John");
+        assertNotNull(player);
+        assertEquals("John", player.getName());
+        assertNull(player.getCards());
+        assertEquals(0, player.getPoints());
+    }
+
+    @Test
+    public void testGetName() {
+        assertEquals("First player", player.getName());
+    }
+
+    @Test
+    public void testAddPoints() {
+        Player player = new Player("John");
+        player.addPoints(5);
+        assertEquals(5, player.getPoints());
+        player.addPoints(3);
+        assertEquals(8, player.getPoints());
+    }
+
+    @Test
+    public void testSetCards() {
+        Player player = new Player("John");
+        player.setCards(Symbol.HED, Symbol.TRE);
+        Set<Symbol> expected = Set.of(Symbol.HED, Symbol.TRE);
+        assertEquals(expected, player.getCards());
+    }
 }
