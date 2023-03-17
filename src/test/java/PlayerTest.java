f78dd43a30a22ad9d7ed191513cb6bd1c0f3dd0e
Michał Baran <michal.baran.dev@gmail.com>
2023-03-17 16:23:54 +0100

    Correcting failed workflow

--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -30,7 +30,7 @@ class PlayerTest {
 
     @Test
     public void testAddPoints() {
-        player.addPoints(1);
+        player.addPoints(3);
         assertEquals(3, player.getPoints());
         player.addPoints(5);
         assertEquals(8, player.getPoints());
